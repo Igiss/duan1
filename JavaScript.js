844c92758584c5a8172cda7acccf33761bc81624
Master <nhniem1231@gmail.com>
2024-10-25 20:18:59 +0700

    sua quan trong lan 17

--- JavaScript.js
+++ JavaScript.js
@@ -25,31 +25,3 @@ document.addEventListener("DOMContentLoaded", () => {
 
   setActiveTab("IVY moda"); // Khởi tạo với IVY moda là tab mặc định
 });
-
-document.addEventListener("DOMContentLoaded", () => {
-  const tabModa = document.getElementById("tab-ivy-moda-FALL");
-  const tabMen = document.getElementById("tab-ivy-men-FALL");
-  const contentModa = document.getElementById("content-ivy-moda-FALL");
-  const contentMen = document.getElementById("content-ivy-men-FALL");
-
-  function setActiveTab(tab) {
-    tabModa.classList.remove("tab-active-arrival-tab");
-    tabMen.classList.remove("tab-active-arrival-tab");
-
-    contentModa.style.display = "none";
-    contentMen.style.display = "none";
-
-    if (tab === "IVY moda") {
-      tabModa.classList.add("tab-active-arrival-tab");
-      contentModa.style.display = "block";
-    } else if (tab === "IVY men") {
-      tabMen.classList.add("tab-active-arrival-tab");
-      contentMen.style.display = "block";
-    }
-  }
-
-  tabModa.addEventListener("click", () => setActiveTab("IVY moda"));
-  tabMen.addEventListener("click", () => setActiveTab("IVY men"));
-
-  setActiveTab("IVY moda"); // Khởi tạo với IVY moda là tab mặc định
-});
